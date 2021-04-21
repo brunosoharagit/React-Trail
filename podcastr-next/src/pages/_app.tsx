35d242c8896d221534f5d43a964e531e0c424ca2
brunosoharagit <b.sohara.git@gmail.com>
2021-04-21 14:06:26 -0300

    NLWREACT 2021.04.21 - Player: adição de componentes do player

--- podcastr-next/src/pages/_app.tsx
+++ podcastr-next/src/pages/_app.tsx
@@ -1,13 +1,18 @@
-import {Header} from '../components/Header'
-import style from '../styles/app.module.scss'
+import '../styles/global.scss';
+import styles from '../styles/app.module.scss';
 
-import '../styles/global.scss'
+
+import {Header} from '../components/Header';
+import { Player } from '../components/Player';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <Header />
-      <Component {...pageProps} />
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
     </div>
   ) 
 }
